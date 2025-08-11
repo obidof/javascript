806508781196c874119865cf1e9951c0dc73293e
obidof <nasimxonobidov5@gmail.com>
2025-08-11 15:52:35 +0500

    dom 5-dars

--- dom 5-dars/app.js
+++ dom 5-dars/app.js
@@ -1,53 +1,46 @@
 // const text = document.getElementById('text')
+// const getbtn = document.getElementById('getbtn')
 // const check = document.getElementById('check')
 // const color = document.getElementById('color')
-// const btn = document.getElementById('btn')
 // const select = document.getElementById('select')
 
 // check.onchange = (e) => {
-//     console.log(e.target.checked);
+//     console.log(e.target.checked)
 //     if (e.target.checked) document.body.style.background = 'black'
 //     else {
 //         document.body.style.background = 'white'
 //     }
 // }
-
-// console.log(check);
-
-
 // color.oninput = (e) => {
-//     console.log(e.target.value);
+//     console.log(e.target.value)
 //     title.style.color = e.target.value;
 // }
 
-// select.onchange = (e) => {
-//     console.log(e.target.value);
 
-// }
 
-// const form = document.getElementById('form')
-// const formdata = document.getElementById('formdata')
-
+// const form = document.getElementById('form');
+// const formdata = document.getElementById('formdata');
 
 // select.onchange = (e) => {
 //     let id = e.target.value
-//     console.log(id);
+//     console.log(id)
+//     form.action = https://jsonplaceholder.typicode.com/posts/${id}
+//     form.method = 'get';
 
-//     form.action = `https://jsonplaceholder.typicode.com/posts/${id}`
-//     form.method = 'get'
 // }
+
 
 const users = [
     {
         id: 1,
-        name: "nasimxon",
-        fio: "obidof",
+        name: 'Nasimhon',
+        fio: "Obidof",
         age: "16"
     },
     {
         id: 2,
-        name: "rahmadjon",
-        fio: "abdullayev",
+        name: 'Rahmadjon',
+        fio: "Abdullayev",
         age: "1"
     },
 ]
@@ -56,41 +49,46 @@ const ism = document.getElementById('ism')
 const fio = document.getElementById('fio')
 const age = document.getElementById('age')
 
-const sumbit = document.getElementById('sumbit')
-const data = document.getElementById('data')
+const sumbit = document.getElementById('sumbit');
 
-form.onsumbit = (e) => {
+const data = document.getElementById('data');
+
+form.onsubmit = (e) => {
     e.preventDefault()
     let newUser = {
         id: users.length + 1,
         name: ism.value,
         fio: fio.value,
-        age: age.valeu
+        age: age.value
     }
     users.push(newUser)
-
+   
     getUsers()
+
 }
 
-function getUsers() {   
+
+function getUsers() {
     data.innerHTML = ''
     users.map((item) => {
-        console.log(item);
-        const box = document.createElement('div')
+        const box = document.createElement('div');
         const title = document.createElement('h1')
         const familiya = document.createElement('h2')
         const para = document.createElement('p')
         title.textContent = `${item.id} - ${item.name}`
         familiya.textContent = item.fio
         para.textContent = item.age
-
         box.classList.add('user')
         box.append(title)
         box.append(familiya)
         box.append(para)
         data.append(box)
-        console.log(data);
-        
+        console.log(data)
     })
 }
+
+
 getUsers()
+
+
+
--- hisobchi/app.js
+++ hisobchi/app.js
@@ -1,3 +1,22 @@
-const data  = document.getElementById('data')
-const result = document.getElementsById('result')
+const birthdateInput = document.getElementById('birthdate');
+    const result = document.getElementById('result');
 
+    birthdateInput.addEventListener('change', () => {
+      const birthDate = new Date(birthdateInput.value);
+      const today = new Date();
+
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const m = today.getMonth() - birthDate.getMonth();
+
+      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      if (!isNaN(age)) {
+        result.textContent = `result: ${age} yosh`;
+      } else {
+        result.textContent = `result: 0 yosh`;
+      }
+
+      
+    });
