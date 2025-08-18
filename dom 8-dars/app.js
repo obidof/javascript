eda34f969bf783718eeece20006cc1b766554d7d
obidof <nasimxonobidov5@gmail.com>
2025-08-18 17:28:39 +0500

    ....................

--- dom 8-dars/app.js
+++ dom 8-dars/app.js
@@ -1,10 +1,44 @@
 const result = document.querySelector('.result')
 const btns = document.querySelectorAll('.btn')
+const errormsg = document.querySelector('.errormsg')
+
+errormsg.style.display = 'none'
+
+let sum = ''
+
+result.oninput = (e)=>{
+    sum = e.target.value
+}
 
 btns.forEach((btn) => {
-    btn.addEventListener('click', () =>  {
-        let btnValue = btn.textContent;
-        sum += btnValue
-        result.value = sum
+    let btnValue = btn.textContent;
+    btn.addEventListener('click', () => {
+
+        if (btnValue == 'C' || btnValue == '.' || btnValue == '=' || btn.className == 'btn back') {
+        }
+
+        if (btnValue == 'C') {
+            result.value = ''
+            sum = ''
+        }
+
+        if (btnValue == '=') {
+
+            try {
+                result.value = eval(sum.replaceAll('x', '*'))
+                sum = result.value;
+                console.log(sum)
+            } catch (err) {
+                errormsg.style.display = 'block'
+            }
+        }
+
+        else {
+            sum += btnValue
+            result.value = sum
+        }
+
     })
+
 })
+
