7c330b0a5df31c163f7dcf3e016e5c9a201e45d2
obidof <nasimxonobidov5@gmail.com>
2025-09-15 18:48:39 +0500

    system

---
+++ sotuv sistem/ombor.js
@@ -0,0 +1,16 @@
+const savdoInt = document.querySelector('.savdo-int')
+const savdoInt1 = document.querySelector('.savdo-int1')
+const savdoInt2 = document.querySelector('.savdo-int2')
+const savdoInt3 = document.querySelector('.savdo-int3')
+const add = document.querySelector('.add')
+
+add.onclick = () => {
+    console.log(
+        obj = {
+            name: savdoInt.value,
+            size: savdoInt1.value,
+            soni: savdoInt2.value,
+            price: savdoInt3.value,
+        }
+    )
+}
